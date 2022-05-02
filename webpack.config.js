612bc4d80c9adcd7d0a34b2b08ba6da09cee57e6
Louis Charette <charette.louis@gmail.com>
2022-05-01 22:34:05 -0400

    Update frontend implementation

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.UF_MODE || 'dev');
+}
+
+Encore
+    .setOutputPath('public/assets')
+    .setPublicPath('/assets/')
+    .addEntry('app', './app/assets/main.js')
+    .addEntry('page.register', './node_modules/theme-adminlte/app/assets/register.js')
+    .addEntry('page.sign-in', './node_modules/theme-adminlte/app/assets/sign-in.js')
+    .addEntry('page.forgot-password', './node_modules/theme-adminlte/app/assets/forgot-password.js')
+    .addEntry('page.resend-verification', './node_modules/theme-adminlte/app/assets/resend-verification.js')
+    .addEntry('page.set-or-reset-password', './node_modules/theme-adminlte/app/assets/set-or-reset-password.js')
+    .addEntry('page.account-settings', './node_modules/theme-adminlte/app/assets/account-settings.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+    .autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
