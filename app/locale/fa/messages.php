061fe7c163f0ccc3acdf7114953f6bde0a9122d0
Louis Charette <charette.louis@gmail.com>
2024-03-22 21:59:51 -0400

    Update copyright

--- app/locale/fa/messages.php
+++ app/locale/fa/messages.php
@@ -4,7 +4,7 @@
  * UserFrosting Admin Sprinkle (http://www.userfrosting.com)
  *
  * @link      https://github.com/userfrosting/sprinkle-admin
- * @copyright Copyright (c) 2022 Alexander Weissman & Louis Charette
+ * @copyright Copyright (c) 2013-2024 Alexander Weissman & Louis Charette
  * @license   https://github.com/userfrosting/sprinkle-admin/blob/master/LICENSE.md (MIT License)
  */
 
