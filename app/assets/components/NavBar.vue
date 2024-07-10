372381f10231e12ce285b499fc5cb52ee7b1a697
Louis Charette <charette.louis@gmail.com>
2024-07-10 19:23:52 -0400

    Add NavBar User Card

--- app/assets/components/NavBar.vue
+++ app/assets/components/NavBar.vue
@@ -1,7 +1,23 @@
+<script setup>
+import { useLogoutApi } from '@userfrosting/sprinkle-account/api'
+import { useAuthStore } from '@userfrosting/sprinkle-account/stores'
+
+// Logout API variables
+const auth = useAuthStore()
+const { logout } = useLogoutApi(auth)
+</script>
+
 <template>
     <UFNavBar title="Userfrosting">
         <UFNavBarItem :to="{ name: 'home' }" label="Home" />
         <UFNavBarItem :to="{ name: 'about' }" label="About" />
         <UFNavBarItem :to="{ name: 'auth' }" label="Auth" />
+        <UFNavBarUserCard
+            v-if="auth.isAuthenticated"
+            :username="auth.user.full_name"
+            :avatar="auth.user.avatar"
+            :meta="auth.user.user_name">
+            <UFNavBarUserCardButton label="Logout" @click="logout()" />
+        </UFNavBarUserCard>
     </UFNavBar>
 </template>
