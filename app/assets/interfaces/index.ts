06d991dc03cc7571743d9ee4454e57dc8fb5ced8
Louis Charette <charette.louis@gmail.com>
2024-10-19 22:39:12 -0400

    Add Role and Permissions interfaces

--- app/assets/interfaces/index.ts
+++ app/assets/interfaces/index.ts
@@ -1,6 +1,15 @@
 import type { LoginForm } from './loginForm'
 import type { UserInterface } from './userInterface'
 import type { GroupInterface } from './groupInterface'
+import type { RoleInterface } from './roleInterface'
+import type { PermissionInterface } from './permissionInterface'
 import type { RouteGuard } from './routes'
 
-export type { LoginForm, UserInterface, GroupInterface, RouteGuard }
+export type {
+    GroupInterface,
+    LoginForm,
+    PermissionInterface,
+    RoleInterface,
+    RouteGuard,
+    UserInterface
+}
--- dist/interfaces/index.d.ts
+++ dist/interfaces/index.d.ts
@@ -1,5 +1,7 @@
 import { LoginForm } from './loginForm';
 import { UserInterface } from './userInterface';
 import { GroupInterface } from './groupInterface';
+import { RoleInterface } from './roleInterface';
+import { PermissionInterface } from './permissionInterface';
 import { RouteGuard } from './routes';
-export type { LoginForm, UserInterface, GroupInterface, RouteGuard };
+export type { GroupInterface, LoginForm, PermissionInterface, RoleInterface, RouteGuard, UserInterface };
