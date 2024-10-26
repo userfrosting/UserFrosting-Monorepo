c2fee03275489d366d81148a402b53306d14bb2e
Louis Charette <charette.louis@gmail.com>
2024-10-26 13:39:18 -0400

    Add VueDevTools Plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vueDevTools({
+            appendTo: 'app/assets/main.ts',
+        })
+    ],
     server: {
         strictPort: true,
         port: 3000,
