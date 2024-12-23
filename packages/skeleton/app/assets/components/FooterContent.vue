bfcf4bbf97331770d6eacd3814f72900233dffb8
Louis Charette <charette.louis@gmail.com>
2024-12-22 21:20:01 -0500

    Update format

--- packages/skeleton/app/assets/components/FooterContent.vue
+++ packages/skeleton/app/assets/components/FooterContent.vue
@@ -2,8 +2,8 @@
     <UFFooterContent>
         Copyright 2024 - <a href="https://www.userfrosting.com">Created with UserFrosting</a> |
         Theme built with
-        <a href="http://getuikit.com" title="Visit UIkit 3 site" target="_blank" data-uk-tooltip
-            ><span data-uk-icon="uikit"></span
-        ></a>
+        <a href="http://getuikit.com" title="Visit UIkit 3 site" target="_blank" data-uk-tooltip>
+            <span data-uk-icon="uikit"></span>
+        </a>
     </UFFooterContent>
 </template>
